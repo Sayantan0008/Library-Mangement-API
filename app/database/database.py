1502cd3f560e9de3fa90477c91ab12b57c56861b
Sayantan0008 <138349965+Sayantan0008@users.noreply.github.com>
2025-01-27 15:32:58 +0530

    Delete app directory

--- app/database/database.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-def get_db_connection():
-    conn = sqlite3.connect('library.db')
-    conn.row_factory = sqlite3.Row
-    return conn
-
-def close_db_connection(conn):
-    conn.close()
